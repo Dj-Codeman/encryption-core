98e32f7f90fa62c21d8e2bf4f8774d0dd86181bd
Darrion Whitfield <darrionjw@outlook.com>
2022-06-30 20:18:18 -0500

    thriving

--- test.sh
+++ test.sh
@@ -2,14 +2,20 @@
 
 echo "test data" | doas tee -a ./test.file
 
+
+echo " initializing " 
 doas encore initialize
 
+echo "Writing test"
 doas encore write ./test.file debug debug
 
+echo "Showing vars from json"
 doas encore debug json debug debug
 
+echo "destroying test"
 doas encore destroy debug debug
 
+echo "redownloading"
 doas rm -rfv /tmp/encryption-core 
 
 cd /tmp 
