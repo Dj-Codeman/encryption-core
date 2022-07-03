81824c3ff615049f391863e0ff00180681b15adf
Darrion Whitfield <darrionjw@outlook.com>
2022-07-03 13:23:45 -0500

    making instal.sh more robust

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function relazy {
+    echo "Hello out there" > /dev/null
+}
+
 #Who is running this 
 
 person="$(whoami)"
@@ -13,7 +17,7 @@ if [ -f "/opt/encore/scripts/encore" ]; then
 
     if [ -f "/opt/encore/config" ]; then 
 
-        source "/opt/encore/scripts/function.php"
+        source "/opt/encore/scripts/functions.php"
 
         if [ "$1" == "update" ]; then
 
@@ -34,37 +38,37 @@ if [ -f "/opt/encore/scripts/encore" ]; then
 
             if [[ -f "$test_path" ]]; then 
 
-            echo -e "Key presents verified - checking if its valid \n"
+                echo -e "Key presents verified - checking if its valid \n"
 
-            echo -e "Creating some random data \n"
+                echo -e "Creating some random data \n"
 
-            test_data="$(fetch_keys $test_$key | sed 's/[ -]//g' | base64 | head -c 200; )"
+                test_data="$(fetch_keys $test_$key | sed 's/[ -]//g' | base64 | head -c 200; )"
             
-            echo "$test_data" | doas tee -a /tmp/encore.tmp
+                echo "$test_data" | doas tee -a /tmp/encore.tmp
 
-            encore write /tmp/encore.tmp system test
+                encore write /tmp/encore.tmp system test
 
-            $old_val_lip="$(grep "leave_in_peace=" /opt/encore/config )" 
+                $old_val_lip="$(grep "leave_in_peace=" /opt/encore/config )" 
 
-                if [[ "$old_val_lip" == "leave_in_peace=1" ]]; then    
-                sed -i 's/leave_in_peace=1/leave_in_peace=0/g' /opt/encore/config
-                fi
+                    if [[ "$old_val_lip" == "leave_in_peace=1" ]]; then    
+                        sed -i 's/leave_in_peace=1/leave_in_peace=0/g' /opt/encore/config
+                    fi
         
         
-            encore destroy system test
+                    encore destroy system test
 
-            sed -i 's/leave_in_peace=0/'"'$old_val_lip'"'/g' /opt/encore/config
+                    sed -i 's/leave_in_peace=0/'"'$old_val_lip'"'/g' /opt/encore/config
 
 
-            echo -e "Damn it looks like you installed encore already thank you \n"
-            echo -e "Also from my small test it looks like your installation is file \n"
-            echo -e "Phew. well then what can i do for you"
+                echo -e "Damn it looks like you installed encore already thank you \n"
+                echo -e "Also from my small test it looks like your installation is file \n"
+                echo -e "Phew. well then what can i do for you"
 
-            read options
+                read options
 
-            echo -e "damn $options thats a great choice but uuhhhh actually theres isnt enough code for that \n"
-            echo -e "Sorry"
-            exit 0 
+                echo -e "damn $options thats a great choice but uuhhhh actually theres isnt enough code for that \n"
+                echo -e "Sorry"
+                exit 0 
 
             fi
                 
@@ -93,12 +97,59 @@ if [ -f "/opt/encore/scripts/encore" ]; then
         # re write the privious file
 
         #### WHAT ABOUT THE OLD DIRECTORIES???
+        else
+            relazy
+
+            # proceed to regular install
+            # jr and jp are the only dependencies for json editing
+            # I only use ubuntu and arch btw
+            if [[ -f "/usr/bin/pacman" ]]; then 
+                pacman -Sy jr jp vim
+            elif [[ -f "/usr/bin/apt" ]]; then
+                apt-get -y install jr jp
+            else 
+                echo -e "small problem I dont know how to install dependencies for you"
+                echo -e "all you need is jq and jr for json manipulation xxd and vim-commons"
+                exit 1
+            fi
+
+            if [[ -f /tmp/encryption-core/scripts/encrypt ]]; then
+            relazy
+            else 
+            wget -i https://raw.githubusercontent.com/fastsitephp/fastsitephp/master/scripts/shell/bash/encrypt.sh
+            fi 
+
+            rm -rfv /opt/encore
+
+            mkdir /opt/encore
+
+            mv -v ./* /opt/encore/
+
+            mv -v /opt/encore/test.sh ./test.sh
+
+            chmod +xv /opt/encore/scripts
+
+            ln -s /opt/encore/scripts/encore /usr/local/bin/encore
+
+            ln -s /opt/encore/scripts/encrypt /usr/local/bin/encrypt
+
+            encore initialize
+
+            chown -Rfv $USER:$USER /opt/encore
+
+            clear 
+
+            echo "encore installed / updated sucessfully"
+
+            exit 0
+
         fi
+
     else 
         echo -e "I know whats wrong with it \n"
         echo -e "It aint got no config int \n"
 
-        # exit 1
+        exit 1
 
     fi
 
@@ -111,36 +162,3 @@ echo "Oh no ?"
 # exit 1
 
 fi
-
-
-# jr and jp are the only dependencies for json editing
-# I only use ubuntu and arch btw
-if [[ -f "/usr/bin/pacman" ]]; then 
-pacman -Sy jr jp vim
-elif [[ -f "/usr/bin/apt" ]]; then
-apt-get -y install jr jp
-fi
-
-rm -rfv /opt/encore
-
-mkdir /opt/encore
-
-mv -v ./* /opt/encore/
-
-mv -v /opt/encore/test.sh ./test.sh
-
-chmod +xv /opt/encore/scripts
-
-ln -s /opt/encore/scripts/encore /usr/local/bin/encore
-
-ln -s /opt/encore/scripts/encrypt /usr/local/bin/encrypt
-
-encore initialize
-
-chown -Rfv $USER:$USER /opt/encore
-
-clear 
-
-echo "encore installed / updated sucessfully"
-
-exit 0
