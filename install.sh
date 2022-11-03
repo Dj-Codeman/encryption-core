8bf11e2e231356efcd11e7b8b0358e7ecd7ccd39
Dj-Codeman <dwhitfield@ramfield.net>
2022-11-03 17:26:46 -0500

    Version bug

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 # P is a patched version in development because I dont get how branches work on git yet
 
 major=1
-Nversion="P1.77"
+Nversion="P1.78"
 
 function update() {
     source /opt/encore/scripts/functions.sh
